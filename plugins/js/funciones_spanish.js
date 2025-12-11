acf61f7ccc0a834b39c2c52ea3b844d33fbba636
earodb <earodb@gmail.com>
2025-12-11 00:31:44 -0600

    Se agrega el proyecto

---
+++ plugins/js/funciones_spanish.js
@@ -0,0 +1,290 @@
+/* ***** BEBIDAS ***** */
+document.getElementById('CervezaBarril').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('CervezaBarril').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('CervezaBotella').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('CervezaBotella').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Licores').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Licores').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Cocteles').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Cocteles').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('CervezaArtesanal').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('CervezaArtesanal').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('BebidasMixtas').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('BebidasMixtas').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Litros').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Litros').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Gaseosas').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Gaseosas').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Jugos').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Jugos').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Micheladas').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Micheladas').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Ron').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Ron').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Vodka').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Vodka').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Gin').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Gin').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Tequilas').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Tequilas').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Whisky').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Whisky').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('OtrosLicores').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('OtrosLicores').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('ShotsFrios').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('ShotsFrios').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Promociones').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Promociones').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirBebidas'));
+  modalPrincipal.show();
+});
+
+
+/* ***** COMIDAS ***** */
+document.getElementById('Aperitivos').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Aperitivos').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Fish').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Fish').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('HamCarne').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('HamCarne').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('HamPollo').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('HamPollo').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('Mexicano').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('Mexicano').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
+
+document.getElementById('PapasCargadas').addEventListener('show.bs.modal', function (event) {
+  // Oculta el modal principal cuando el secundario empieza a mostrarse
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.hide();
+});
+
+document.getElementById('PapasCargadas').addEventListener('hidden.bs.modal', function (event) {
+  // Muestra el modal principal cuando el secundario se ha cerrado completamente
+  var modalPrincipal = new bootstrap.Modal(document.getElementById('AbrirComidas'));
+  modalPrincipal.show();
+});
